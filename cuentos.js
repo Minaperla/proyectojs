c41e73d2758177394a8037bcece381dad1f94822
Glenda Bohorquez <Glenda Bohorquez>
2016-03-24 11:23:05 -0500

    Desafio

---
+++ cuentos.js
@@ -0,0 +1,7 @@
+var verbo = prompt ("Ingresa un verbo");
+var adjetivo = prompt ("Ingresa un adjetivo");
+var sustantivo = prompt ("Ingresa un sustantivo");
+document.write("<h1>Te cuento un cuento:</h1><br>");
+document.write("<p>El "+ sustantivo + " " + adjetivo +" "+ verbo +" locamente</p>");
+document.write("<p> "+ verbo + " el " + sustantivo +" "+ adjetivo +" locamente</p>");
+document.write("<p>locamente el "+ adjetivo + " " + sustantivo+" "+verbo+"</p>");
--- proyecto.js
+++ proyecto.js
@@ -1,2 +1,19 @@
+/*console.log ("Inicio de programa");
+var clave = prompt("Hola Usuario, Ingrese su clave");
 alert ("Hola Java Script");
 document.write("<h1> Hola Mundo JS </h1>");
+alert("Gracias por  visitar");
+console.log ("Fin de Programa");
+console.log ("La longitud de la clave es:" + clave.length);*/
+
+console.log ("Inicio de programa");
+var clave = prompt("Ingrese un verbo");
+var clave = prompt("Ingrese un sustantivo");
+var clave = prompt("Ingrese un adjetivo");
+
+document.write (<h1> )
+var 
+
+
+
+
