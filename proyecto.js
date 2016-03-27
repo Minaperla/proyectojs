5787836b9b18c8da813ef5b362033fa412ca75dc
Minaperla <glendysbohorquez@hotmail.es>
2016-03-27 09:29:19 -0500

    Correcciones

--- proyecto.js
+++ proyecto.js
@@ -3,17 +3,20 @@ var clave = prompt("Hola Usuario, Ingrese su clave");
 alert ("Hola Java Script");
 document.write("<h1> Hola Mundo JS </h1>");
 alert("Gracias por  visitar");
-console.log ("Fin de Programa");
+/*console.log ("Fin de Programa");
 console.log ("La longitud de la clave es:" + clave.length);*/
 
-console.log ("Inicio de programa");
-var clave = prompt("Ingrese un verbo");
-var clave = prompt("Ingrese un sustantivo");
-var clave = prompt("Ingrese un adjetivo");
+/*var segundosPorMinuto = 60;
+var minutosPorHora = 60;
+var horasPorDia = 24;
+var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
+document.write("<h1>Calculadora del Tiempo</h1> <p>Hay " segundosPorDia    +" en un dia</p>");*/
 
-document.write (<h1> )
-var 
+console.log ("Inicio de programa")
 
+var peliculasVistas = prompt("Cuantas peliculas haz visto este mes?");
+var seriesVistas = prompt("Cuantas series has visto este mes?");
+var tiempoPantalla =  parseInt (peliculasVistas) + parseInt (seriesVistas);
+alert('Wow! Has visto ' + tiempoPantalla + " veces minimo la pantalla.");
 
-
-
+console.log ("Programa completado")
