607abe778a1d57808d8d895635b8c90245255231
Minaperla <glendysbohorquez@hotmail.es>
2016-03-27 12:16:39 -0500

    Ejercicio de adivinanzas

--- proyecto.js
+++ proyecto.js
@@ -1,9 +1,53 @@
 console.log ("Inicio de programa");
-var dado = Math.floor(Math.random()  *6)  + 1;
-alert("dado dice:"  + dado);
-console.log("Programa completado");
+
+var usuario = prompt("Ingrese un numero del 1 al 10:  ");
+var numero = 7;
+if (parseInt(usuario) === numero)
+{
+adivinoCorrectamente = true;
+} 
+else if ((parseInt(usuario) > numero))
+{
+alert("Te equivocaste amigo");
+var nuevoIntento = prompt("Intenta de nuevo, ingresa otro numero de 1 a 10:  ");
+if((parseInt(nuevoIntento) === numero))
+{
+adivinoCorrectamente = true;
+}
+else
+{
+adivinoCorrectamente = false;
+}
+}
+else if ((parseInt(usuario) < numero))
+{
+alert("Fallasteeeeee...!!!");
+var nuevoIntento = prompt("Ingresa otro numero: ");
+if ((parseInt(nuevoIntento)=== numero))
+{
+adivinoCorrectamente = true;
+}
+else
+{
+adivinoCorrectamente = false;
+}
+}
 
 
+if (adivinoCorrectamente)
+{
+document.write("<p>Adivinaste!!!!</p>");
+
+}
+else
+{
+document.write("<p>NO Adivinaste!!!!</p>");
+}
+console.log("Programa comnpleto");
+
+
+/* var dado = Math.floor(Math.random()  *6)  + 1;
+alert("Dado dice:"  + dado);*/
 /*document.write("<h1> Hola Mundo JS </h1>");
 alert("Gracias por  visitar");
 /*console.log ("Fin de Programa");
